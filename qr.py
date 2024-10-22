c2e6e3aaa4fef2f5b7e14630b9bab51911b52d85
Angel Zarco <91713291+angel-manuel-zarco@users.noreply.github.com>
2024-10-22 07:47:17 -0600

    Ultimos cambios qr.py
    se agregron las ultimas modificaciones del codigo, principalmente los comentarios

--- qr.py
+++ qr.py
@@ -6,14 +6,20 @@ import qrcode #Se importa la libreria que posteriormente instalamos
 # URL que se desea codificar en el QR.
 url = "https://github.com/angel-manuel-zarco"
 
+# Crea una instancia de QRCode con parámetros personalizados.
 qr = qrcode.QRCode(
-    version=1,
-    box_size=25,
-    border=5
+    version=1,      # Controla el tamaño del QR (1 es el más pequeño).
+    box_size=25,    # Tamaño de cada cuadrado del código en píxeles.
+    border=5        # Grosor del borde del QR (en cuadros blancos).
 )
 
+# Agrega la URL al objeto QRCode.
 qr.add_data(url)
+
+# Ajusta automáticamente el tamaño para que encaje con los datos proporcionados.
 qr.make(fit=True)
 
+# Genera la imagen del código QR con los colores deseados.
 imagen = qr.make_image(fill_color="black", back_color="white")
-imagen.save("QR.png")  # Agregamos la extensión .png
+
+# Guarda la imagen generada en fo
