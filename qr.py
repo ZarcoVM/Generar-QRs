2b20ae521baf1a493e5b1fa081fb151a4b65b6cb
Zarco <angelmanuelzarcovalerio@gmail.com>
2024-10-22 01:39:12 -0600

    Generador de QR

---
+++ qr.py
@@ -0,0 +1,15 @@
+import qrcode
+
+url = "https://github.com/angel-manuel-zarco"
+
+qr = qrcode.QRCode(
+    version=1,
+    box_size=25,
+    border=5
+)
+
+qr.add_data(url)
+qr.make(fit=True)
+
+imagen = qr.make_image(fill_color="black", back_color="white")
+imagen.save("QR.png")  # Agregamos la extensión .png
