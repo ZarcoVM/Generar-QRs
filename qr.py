284fa2afe7008e5be434863d12c9e18842542567
Angel Zarco <91713291+angel-manuel-zarco@users.noreply.github.com>
2024-12-11 23:25:04 -0600

    Actualizar qr.py

--- qr.py
+++ qr.py
@@ -4,6 +4,7 @@
 import qrcode #Se importa la libreria que posteriormente instalamos
 
 
+
 # URL que se desea codificar en el QR. esta ruta se cambia, 
 # en mi ejemplo se redirije a mi plataforma de git hub pero 
 # podria ser perferfectanebte cualquier cosa
