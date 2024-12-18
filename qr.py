513ed3aecac0e2a3ebae0450929e68dbf9e72263
Angel Zarco <91713291+angel-manuel-zarco@users.noreply.github.com>
2024-12-17 20:51:01 -0600

    Actualizar qr.py

--- qr.py
+++ qr.py
@@ -1,6 +1,7 @@
 # este es el comando para instalar qrcode 
 # pip install qrcode[pil] 
 
+
 import qrcode #Se importa la libreria que posteriormente instalamos
 
 # URL que se desea codificar en el QR. esta ruta se cambia, 
